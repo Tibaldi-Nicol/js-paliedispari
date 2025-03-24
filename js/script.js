5ed4bc27d01546503a751101c5fac835b7144942
Tibaldi-Nicol <nickkoltibaldi@gmail.com>
2025-03-24 17:01:12 +0100

    [UPD]Done-Function

--- js/script.js
+++ js/script.js
@@ -2,4 +2,7 @@ function checkpalindroma(string){
     let splitted = string.split('');
     let reversed = splitted.reverse();
     let joined = reversed.join('');
+    console.log(splitted);
+    console.log(reversed);
+    console.log(joined);
 }
